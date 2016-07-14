08d9a368c2caed31f541d0b7e45307a2a7b0ccf9
Sunvenger <sunvenger@gmail.com>
2016-07-14 21:58:54 +0200

    Added class "Limired_var" which only allows assign values between min and max

---
+++ limited_type.py
@@ -0,0 +1,37 @@
+from sys import argv
+
+scr_name, input=argv
+class Limited_var:
+	max_=0 # Maximum assignable value
+	min_=0 # Minimum assignable value
+	__value=0
+	def __init__(self,min, max,val):
+		self.max_=max
+		self.min_=min
+		__value=val
+	
+	def display_(self):
+		return self.value
+	
+	def set(self,val):
+		self.__value=max(min(self.max_,val),self.min_);
+		
+	def get(self):
+		return self.__value
+		
+	def __str__(self):
+		return "%r"%self.__value
+		
+	def __int__(self):
+		return self.__value
+		
+	def __add__(self, other):
+		return self.__value + other
+		
+	def __radd__(self, other):
+		return self.__value + other
+lim=Limited_var(0,5,0)
+lim.set(-10)
+a=lim
+print (lim)
+
