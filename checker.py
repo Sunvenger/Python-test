db400bf005ef0353f64da09b3b4bca9751a435a9
sunvenger <sunvenger@gmail.com>
2016-07-20 16:52:31 +0200

    test now testing instance as a parameter

--- checker.py
+++ checker.py
@@ -1,18 +1,19 @@
 from limited_type import Limited_var
 
-limits=[-4,1000] # test limits
-inputs = ["String test",1100,-200,3.1415,-3.1415] # test inputs
-outputs= [0,limits[1],limits[0],inputs[3],inputs[4]] #required outputs
-testvar=Limited_var(limits[0],limits[1],0)
+
+#testvar=Limited_var(limits[0],limits[1],0)
 
 		
-def do_test():
+def do_test(input):
+	limits=[input.min_,input.max_] # test limits
+	inputs = ["String test",1100,-200,3.1415,-3.1415] # test inputs
+	outputs= [0,limits[1],limits[0],inputs[3],inputs[4]] #required outputs
 	fail=0
 	print "********Basic testing started!*********"
 	for i in range(0,len(inputs)) : 
 		print "setting test instance to value %r" % inputs[i]
-		testvar.set(inputs[i])
-		out=testvar.get()
+		input.set(inputs[i])
+		out=input.get()
 		print "output is : %r" % out
 		print "expected is : %r" % outputs[i]
 		if (out==outputs[i]):
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from limited_type import Limited_var
-from checker import inputs,limits,outputs,do_test
+from checker import do_test
 
 print "Main called !"
 
 a=Limited_var(-5,5,1)
 print a
-do_test()
+do_test(a)
