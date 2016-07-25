6e0615e41e295a29fc62f92cbe6af7355adb283c
sunvenger <sunvenger@gmail.com>
2016-07-25 21:30:24 +0200

    Test repair : test now can use pytest

--- checker.py
+++ checker.py
@@ -10,36 +10,12 @@ def do_test(input):
 	inputs = ["String test",input.max_+10,input.min_-10,float(((float(input.max_)+float(input.min_))/2.0)-(float(input.max_)-float(input.min_))/10.0),((input.max_+input.min_)//2),input.min_,input.max_,input.min_-0.0006,input.max_+0.0006] # test inputs
 	outputs= [0,limits[1],limits[0],inputs[3],inputs[4],limits[0],limits[1],limits[0],limits[1]] #required outputs
 	
-	fail=0
+	
 	print "********Basic testing started!*********"
 	for i in range(0,len(inputs)) : 
 		print "%d : %s"%(i+1,test_info[i])
 		print "setting test instance to value %r" % inputs[i]
 		input.set(inputs[i])
 		out=input.get()
-		print "output is : %r" % out
-		print "expected is : %r" % outputs[i]
-		if (type(outputs[i]) is str):
-			print "converting input to string for comparation" 
-			out="%r"%input.get()
-			if (out==(outputs[i])):
-				print "string pass!\n"
-			else :
-				fail=1
-				print "string fail!\n"
-		else:
-			if (out==outputs[i]):
-				print "pass!\n"
-			else:
-				print "fail!\n"
-				fail=1
-				
-	if (fail==1):
-		print "result of test : FAIL"
-	else:
-		print "Test sucessfully passed!"
-	
-	print "------Instance info------"
-	print "minimum limitation : %r"%input.min_
-	print "maximum limitation : %r"%input.max_
-	print "value : %r"%input.get()
+		assert (out==outputs[i]),"result error : excepted value is %r but got %r"%(outputs[i],out)
+	print "sucess"	
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from limited_type import Limited_var
 from checker import do_test
+from colorama import init
 
+init(autoreset=True)
 print "Main called !"
 
 a=Limited_var(-5,57,1)
 print a
 do_test(a)
+
+a=Limited_var(-5,5000,1)
+do_test(a)
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
